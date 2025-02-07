11dda7b6367bd5addd0947117b497f38dcfea521
DevonJoynt <150949698+DevonJoynt@users.noreply.github.com>
2025-02-07 02:57:48 -0500

    Added Pieces

--- Tetris/Assets/Scripts/ScoreManager.cs
+++ Tetris/Assets/Scripts/ScoreManager.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class Score : MonoBehaviour
+public class ScoreManager : MonoBehaviour
 {
-    private int score = 0;
-    public void AddScore(int LinesCleared)
+    public int score = 0; //Set the initial score to 0
+
+    public void AddScore(int linesCleared)
     {
-        switch (LinesCleared)
+        //Switch case for each line clear amount
+        // in the form of a score value
+        switch (linesCleared)
         {
             case 1: score += 100; break;
             case 2: score += 300; break;
             case 3: score += 500; break;
             case 4: score += 800; break;
-
+            default: break;
         }
+
         Debug.Log($"Score: {score}");
     }
 }
